ae78e00e0a984060bfa74d6907dd40915ec958fe
Diego <155783365+T-MWNO-A@users.noreply.github.com>
2024-01-08 21:37:04 -0500

    repository.py

--- .py
+++ .py
@@ -2,4 +2,3 @@ import random
 # Sorprende a tus compañeros
 datos = ["mi aficion favorita es en programar el proyecto que hago", "tengo 4 mascotas, 1 gato siames, 2 perros Shih Tzu y un perro labrador cafe y todas mis mascotas son lindas", "el proyecto en la cual me siento mas orgulloso es en una modificacion de un juego de ritmo para mis amigos en la cual aun sigo trabajando en eso", "mi plato favorito es la causa"] 
 print(random.choice(datos))
-
